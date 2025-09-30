5fbd1b3d42f9cd97d593919c6482237a0eee4bd5
Kenan Curevac <kenancurevac@gmail.com>
2025-09-30 14:48:14 +0200

    game page scroll and home height on small scr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,20 @@ const pinyon = Pinyon_Script({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   return (
-    <div>
-      <div className="min-h-screen bg-[url('/earth-at-night.jpg')] bg-cover bg-center ">
-        <div className="flex flex-col items-center mx-auto h-screen">
-          <Image
-            src="/logo-ptp.png"
-            width={647}
-            height={630}
-            alt="Pin the Place Logo"
-            className="w-44 md:w-52 lg:w-60 py-10"
-          />
-          <PlayButton type="First" />
-          <div
-            className={`text-7xl lg:text-8xl text-white mb-20 ${pinyon.className}`}
-          >
-            Pin the Place
-          </div>
+    <div className="h-[100dvh] bg-[url('/earth-at-night.jpg')] bg-cover bg-center ">
+      <div className="flex flex-col items-center mx-auto h-screen">
+        <Image
+          src="/logo-ptp.png"
+          width={647}
+          height={630}
+          alt="Pin the Place Logo"
+          className="w-44 md:w-52 lg:w-60 py-10"
+        />
+        <PlayButton type="First" />
+        <div
+          className={`text-7xl lg:text-8xl text-white mb-20 ${pinyon.className}`}
+        >
+          Pin the Place
         </div>
       </div>
     </div>
